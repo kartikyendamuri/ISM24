6152b2c4114b2972fe45984af81142ea70568646
Kartik Yendamuri <kartikyendamuri@gmail.com>
2025-11-23 16:25:57 -0500

    Clean code and add final comments

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ number = input("Enter a number: ")
 try:
     # convert to float and perform a calculation
     value = float(number)
+
+    # Basic calculation (example: multiply by 5)
     result = value*5
 
     # Display result
     print(f"Your number multiplied by 5 is: {result}")
 
 except ValueError:
+    # Error message if the input can't be converted
     print("Please enter a valid number next time.")
-    
