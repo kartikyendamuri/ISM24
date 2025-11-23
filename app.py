67cd86726adbd9928256c383c499f03f6d132dc3
Kartik Yendamuri <kartikyendamuri@gmail.com>
2025-11-23 16:24:56 -0500

    Add error handling for non-numeric input

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ print("Welcome to my simple calculator!")
 # Ask user for number
 number = input("Enter a number: ")
 
-# convert to float and perform a calculation
-value = float(number)
-result = value*5
+try:
+    # convert to float and perform a calculation
+    value = float(number)
+    result = value*5
 
-# Display result
-print(f"Your number multiplied by 5 is: {result}")
+    # Display result
+    print(f"Your number multiplied by 5 is: {result}")
+
+except ValueError:
+    print("Please enter a valid number next time.")
+    
