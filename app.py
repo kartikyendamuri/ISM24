0126e4eb75aed1ff90dc40e11e98217e76300418
Kartik Yendamuri <kartikyendamuri@gmail.com>
2025-11-23 16:21:39 -0500

    Add conversion and calculation to program

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ print("Welcome to my simple calculator!")
 
 # Ask user for number
 number = input("Enter a number: ")
+
+# convert to float and perform a calculation
+value = float(number)
+result = value*5
+
